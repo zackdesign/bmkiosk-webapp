893b7677e2ddbe3b85d5e4dc87ea6954cafb9bbb
chris2@sharpwebservice.com <chris2@sharpwebservice.com@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-05-24 00:55:33 +0000

    initial upload

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,72 @@
+class AdminController < ApplicationController
+
+  layout 'personal'
+
+  before_filter :authorize
+  
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @products = Product.find(:all)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def orders
+    @orders =  Orders.find(:all)
+  end
+
+  def view_order
+    @order = Order.find(params[:id])
+    @line_items = @order.line_items
+    @total = 0
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      flash[:notice] = 'Product was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      flash[:notice] = 'Product was successfully updated.'
+      redirect_to :action => 'show', :id => @product
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+  
+  def get_product(id)
+    @product = Product.find(params[:id] = id)
+    @product.title
+  end
+  
+end
