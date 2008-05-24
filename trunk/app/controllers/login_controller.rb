8d913d47ce2aa831809d15efaf600ed56eada565
chris2@sharpwebservice.com <chris2@sharpwebservice.com@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-05-24 00:56:19 +0000

    initial upload

---
+++ trunk/app/controllers/login_controller.rb
@@ -0,0 +1,86 @@
+class LoginController < ApplicationController
+  require 'RMagick'
+  include Magick
+
+  layout 'personal'
+
+  
+  
+  before_filter :find_cart, :except => :empty_cart
+  before_filter :authorize, :except => [:login, :logout]
+  
+  def index
+    @all_users = User.find(:all)
+  end
+  
+
+  # just display the form and wait for user to
+  # enter a name and password
+  
+  def login
+    session[:user_id] = nil
+    session[:user_type] = nil
+    if request.post?
+      user = User.authenticate(params[:password])
+      if user
+        session[:user_id] = user.id
+        session[:user_type] = user.usertype
+        uri = session[:original_uri]
+        session[:original_uri] = nil
+        
+        case user.usertype
+         when "1"    then redirect_to(uri || { :action => "index" })
+         when "2"    then redirect_to(:controller => "phones" , :action => "index" )
+         when "3"    then redirect_to(:controller => "phones" , :action => "index" )
+         else return "Unknown"
+        end 
+      else
+        flash.now[:notice] = "Invalid password..."
+      end
+    end
+  end
+  
+
+  
+  def add_user
+    @user = User.new(params[:user])
+    if request.post? and @user.save
+      flash.now[:notice] = "User #{@user.name} created"
+      @user = User.new
+    end
+  end
+
+  # . . .
+  
+
+  def delete_user
+    id = params[:id]
+    if id && user = User.find(id)
+      begin
+        user.destroy
+        flash[:notice] = "User #{user.name} deleted"
+      rescue Exception => e
+        flash[:notice] = e.message
+      end
+    end
+    redirect_to(:action => :list_users)
+  end
+
+  def list_users
+    @all_users = User.find(:all)
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to(:action => "login")
+  end
+  
+  
+  private
+  
+  def find_cart
+    @cart = (session[:cart] ||= Cart.new)
+  end
+
+end
