abf66409f96fc77b8a7823199514f51fd5cdd5c7
chris2@sharpwebservice.com <chris2@sharpwebservice.com@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-05-24 00:56:19 +0000

    initial upload

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,71 @@
+class StoreController < ApplicationController
+
+  layout 'personal'
+  
+  before_filter :find_cart, :except => :empty_cart
+
+  def index
+    @products = Product.find_products_for_sale
+  end
+  
+  
+  def add_to_cart
+    begin                     
+      product = Product.find(params[:id])  
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product")
+    else
+      @current_item = @cart.add_product(product)
+      redirect_to_index unless request.xhr?         
+    end
+  end
+  
+  def remove_from_cart
+    begin                     
+      product = Product.find(params[:id])  
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product")
+    else
+      @current_item = @cart.remove_product(product)
+      redirect_to_index unless (request.xhr? || hide == 1)     
+    end
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index
+  end
+  
+  def checkout
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty")
+    else
+      @order = Order.new
+    end
+  end
+  
+  def save_order    
+    @order = Order.new(params[:order])   
+    @order.add_line_items_from_cart(@cart)
+    if @order.save                       
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order")
+    else
+      render :action => :checkout
+    end
+  end
+
+  private
+  
+  def find_cart
+    @cart = (session[:cart] ||= Cart.new)
+  end
+  
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg
+    redirect_to :action => :index
+  end
+  
+end
