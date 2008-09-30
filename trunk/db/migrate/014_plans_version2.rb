e6447c88d959e1c1e12f4fdc7ebd48be4bdcb94d
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-09-30 12:17:10 +0000

    Fixed up config and log. Did not add config file for database though...

--- trunk/db/migrate/014_plans_version2.rb
+++ trunk/db/migrate/014_plans_version2.rb
@@ -1,14 +1,14 @@
 class PlansVersion2 < ActiveRecord::Migration
   def self.up
-    change_column :plans, :id, :integer, { :limit => 24 }
+    change_column :plans, :id, :integer, :limit => 24
     change_column :plans, :name, :string
     execute "ALTER TABLE plans ADD COLUMN categories SET('consumer','business','corporate') NOT NULL DEFAULT 'consumer' AFTER name"
     rename_column :plans, :flexi_code, :code
     rename_column :plans, :contract, :period
     execute "ALTER TABLE plans MODIFY COLUMN price DECIMAL(9,2) NULL DEFAULT NULL"
     rename_column :plans, :price, :handset_cost
-    add_column :plans, :handset, :integer, { :limit => 24 }
-    add_column :plans, :plan_group, :integer, { :limit => 24 }
+    add_column :plans, :handset, :integer, :limit => 24
+    add_column :plans, :plan_group, :integer,:limit => 24
     execute "ALTER TABLE plans MODIFY COLUMN flexi_price DECIMAL(9,2) NULL DEFAULT NULL"
     rename_column :plans, :flexi_price, :offer_price
     change_column :plans, :offer, :string
@@ -19,10 +19,10 @@ class PlansVersion2 < ActiveRecord::Migration
   def self.down
     change_column :plans, :id, :integer
     change_column :plans, :name, :text
-    remove_column :plans, :categories
-    rename_column :plans, :code, :flexi_code
-    rename_column :plans, :period, :contract
-    rename_column :plans, :handset_cost, :price
+    #remove_column :plans, :categories
+    #rename_column :plans, :code, :flexi_code
+    #rename_column :plans, :period, :contract
+    #rename_column :plans, :handset_cost, :price
     change_column :plans, :price, :integer
     remove_column :plans, :handset
     remove_column :plans, :plan_group
@@ -30,6 +30,6 @@ class PlansVersion2 < ActiveRecord::Migration
     change_column :plans, :flexi_price, :integer
     change_column :plans, :offer, :text
     add_column :plans, :base_etc, :integer
-    remove_column :plans, repayments
+    remove_column :plans, :repayments
   end
 end
