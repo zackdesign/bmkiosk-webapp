3d0b39acf650a1e141f99987e717ddbe26948589
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-11-05 04:02:08 +0000

    Kiosk view!

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -32,7 +32,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.connect '', :controller => "phones", :action => 'offers', :id => '1'
+  map.connect '', :controller => "kiosk", :action => 'touch', :id => '1'
 
   # See how all your routes lay out with "rake routes"
   map.connect 'phones/list/offers', :controller => 'phones', :action => 'offers', :id => '1'
