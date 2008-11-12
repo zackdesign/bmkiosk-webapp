7b868c319f0fc3596e646a399f28d3e1a59b4a63
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-11-12 20:41:12 +0000

    Phone view

--- app/helpers/kphones_helper.rb
+++ app/helpers/kphones_helper.rb
@@ -4,7 +4,7 @@ module KphonesHelper
           image = Magick::Image.from_blob(feature.picture_data).first
           
           max_dimension = (image.columns < image.rows) ? image.rows : image.columns
-                    if max_dimension < 5
+                    if max_dimension < 50
                       thumb = image
                     else
                       thumb = image.resize_to_fit(50, 50)
@@ -14,5 +14,23 @@ module KphonesHelper
     end
     
     image_tag("/db_images/features/"+feature.picture_name)
+  end
+  
+  def show_phone_thumbnail_if_available(phone)
+    
+     unless FileTest.exist?("public/db_images/features/#{phone.picture_name}")
+          image = Magick::Image.from_blob(phone.picture_data).first
+          
+          max_dimension = (image.columns < image.rows) ? image.rows : image.columns
+                    if max_dimension < 300
+                      thumb = image
+                    else
+                      thumb = image.resize_to_fit(300,300)
+          end
+          
+          File.open('public/db_images/phones/'+phone.picture_name,'w'){|f| f.write(thumb.to_blob)}    end
+    
+    image_tag("/db_images/phones/"+phone.picture_name)
+  
   end
 end
