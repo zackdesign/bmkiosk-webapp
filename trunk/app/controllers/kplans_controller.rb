26d6eb80154518d79f32a83ef282f5ebca6ef805
softwud <softwud@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2008-11-14 06:26:51 +0000

    2008-11-13 Uploaded a third version of the plan search which now displays a summary of the phone chosen (if there is one - otherwise a link to the page to choose a phone is displayed), the plan chosen, and the charges information for that plan.
    If both a phone and plan have been chosen then a link is displayed at the bottom called "Print and Finish" that simply calls the window.print() javascript function to print the page.
    
    Added a default "index" action to kplans_controller.rb so that a user can start by searching plans before then selecting a phone by going to kplans/ (no action or id). Also fixed the layouts/kiosk_inside.html.erb layout so that no error is shown if there is no phone id selected - it simply doesn't display the phone name and network type.

--- trunk/app/controllers/kplans_controller.rb
+++ trunk/app/controllers/kplans_controller.rb
@@ -4,6 +4,24 @@ class KplansController < ApplicationController
 
   layout 'kiosk_inside'
 
+  def index
+    @phone = nil
+
+    @plan_groups_consumer = PlanGroup.find_all_by_categories("consumer")
+    if @plan_groups_consumer.nil?
+      @plan_groups_consumer = Array.new
+    end
+    @plan_groups_business = PlanGroup.find_all_by_categories("business")
+    if @plan_groups_business.nil?
+      @plan_groups_business = Array.new
+    end
+
+    respond_to do |format|
+      format.html { render :action => "list" }
+      format.xml  { render :xml => @phone }
+    end
+  end
+
   def list
     @phone = Phone.find(params[:id])
 
@@ -11,18 +29,27 @@ class KplansController < ApplicationController
     if @plan_groups_consumer.nil?
       @plan_groups_consumer = Array.new
     end
-#    @plans_consumer = Array.new
-#    @plan_groups_consumer.each do |plan_group|
-#      @plans_in_group = plan_group.plans
-#    end
-#    @plan_groups_consumer
     @plan_groups_business = PlanGroup.find_all_by_categories("business")
     if @plan_groups_business.nil?
       @plan_groups_business = Array.new
     end
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
+      format.xml  { render :xml => @phone }
+    end
+  end
+
+  def summary
+    unless params[:phone_id].nil?
+      @phone = Phone.find(params[:phone_id])
+    else
+      @phone = nil
+    end
+    @plan = Plan.find(params[:plan_id])
+
+    respond_to do |format|
+      format.html
       format.xml  { render :xml => @phone }
     end
   end
