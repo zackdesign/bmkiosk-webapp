78e363e0ef9757427b1858e3779a4121503e5f38
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2009-02-17 02:45:32 +0000

    Feature pics smaller

--- app/helpers/kphones_helper.rb
+++ app/helpers/kphones_helper.rb
@@ -5,10 +5,10 @@ module KphonesHelper
           image = Magick::Image.from_blob(feature.picture_data).first
           
           max_dimension = (image.columns < image.rows) ? image.rows : image.columns
-                    if max_dimension < 50
+                    if max_dimension < 15
                       thumb = image
                     else
-                      thumb = image.resize_to_fit(50, 50)
+                      thumb = image.resize_to_fit(15,15)
           end
           
           File.open('public/db_images/features/'+feature.picture_name,'w'){|f| f.write(thumb.to_blob)}
