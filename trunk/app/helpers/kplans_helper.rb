47e65e4347e873850fcba75149f3d0093e66de47
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2009-04-06 10:08:40 +0000

    Netbeans IDE 6.1 project && MRO updates

--- trunk/app/helpers/kplans_helper.rb
+++ trunk/app/helpers/kplans_helper.rb
@@ -1,3 +1,8 @@
 module KplansHelper
-
+  def show_thumbnail_if_available(phone)
+    unless phone.picture_name.blank?
+      image_tag(url_for({ :action => 'thumbnail', :id => phone.id }), :alt => phone.name, :class => "thumb")
+    end
+  end
+  
 end
