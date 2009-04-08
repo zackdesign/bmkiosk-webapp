d768f93f5676a1050fb3bc16454850294b18c66d
zack99 <zack99@c8876725-6b43-0410-b1a3-b16a45ef0afe>
2009-04-08 05:32:39 +0000

    New featured, latest, and popular logic to finish up!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,11 @@ module ApplicationHelper
   
     max = options[:max]
     
-    accessories = Accessory.find(:all, :order => "created_at DESC", :limit => max)
+    accessories = Accessory.find_by_sql('SELECT p.id, p.name
+                                    FROM featured_accessories AS f, accessories AS p
+                                    WHERE p.id = f.accessory_id AND f.atype = "latest"
+                                    ORDER BY p.name
+                                    LIMIT '+max.to_s)
     
     list = accessories[0...accessories.length].map { |a|
             link = link_to "#{a.name}", :controller=>'accessories',:action => 'show', :id => a.id
@@ -23,7 +27,7 @@ module ApplicationHelper
     phones = Phone.find(:all, :conditions => "outofstock = '0' AND discontinued = '0'", :order => "created_at DESC", :limit => max)
     
     list = phones[0...phones.length].map { |a|
-            link = link_to "#{a.name}", :controller=>'phones',:action => 'show', :id => a.id
+            link = link_to "#{a.brand} #{a.name}", :controller=>'phones',:action => 'show', :id => a.id
             "<li>#{link}</li>"
           }
           
@@ -35,15 +39,31 @@ module ApplicationHelper
     
       max = options[:max]
       
-      phones = Phone.find_by_sql('SELECT p.id, p.name
-                                  FROM kiosks AS k, phones AS p
-                                  WHERE k.kiosk < "4"
-                                  AND p.id = k.phone_id                                  
-                                  ORDER BY p.name ASC
+      phones = Phone.find_by_sql('SELECT p.id, p.name, p.brand
+                                  FROM phones AS p                               
+                                  ORDER BY p.popularity DESC
                                   LIMIT ' + max.to_s )
       
       list = phones[0...phones.length].map { |a|
-              link = link_to "#{a.name}", :controller=>'phones',:action => 'show', :id => a.id
+              link = link_to "#{a.brand} #{a.name}", :controller=>'phones',:action => 'show', :id => a.id
+              "<li>#{link}</li>"
+            }
+            
+      return list
+    
+  end
+  
+  def show_popular_accessories(options={:max => 10})
+    
+      max = options[:max]
+      
+      accessories = Accessory.find_by_sql('SELECT p.id, p.name
+                                  FROM accessories AS p                               
+                                  ORDER BY p.popularity DESC
+                                  LIMIT ' + max.to_s )
+      
+      list = accessories[0...accessories.length].map { |a|
+              link = link_to "#{a.name}", :controller=>'accessories',:action => 'show', :id => a.id
               "<li>#{link}</li>"
             }
             
